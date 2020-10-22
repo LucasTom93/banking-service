f2b9b7c34a1073e7d41cf2039449f0e2b28b6131
lukasz.tomczyk <lukasz.tomczyk93@gmail.com>
2020-10-22 17:00:16 +0200

    Updated LoanRequest entity

--- src/main/java/com/asc/loanservice/domain/loan/LoanRequest.java
+++ src/main/java/com/asc/loanservice/domain/loan/LoanRequest.java
@@ -1,5 +1,9 @@
 package com.asc.loanservice.domain.loan;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -10,18 +14,62 @@ class LoanRequest {
     @Column(name = "LOAN_RESUEST_NUMBER")
     private String loanRequestNumber;
     @Column(name = "LOAN_REQUEST_TAX_ID", nullable = false)
-    private double loanRequestTax;
+    private Double loanRequestTax;
+    @Column(name = "LOAN_AMOUNT", nullable = false)
+    private BigDecimal loanAmount;
+    @Column(name = "NUMBER_OF_INSTALLMENTS", nullable = false)
+    private int numberOfInstallments;
+    @Column(name = "FIRST_INSTALLMENT_DATE", nullable = false)
+    private LocalDate firstInstallmentDate;
+    @Column(name = "REGISTRATION_DATE", nullable = false)
+    private LocalDate registrationDate;
 
     LoanRequest() {
         //for JPA
     }
 
-    private LoanRequest(String loanRequestNumber, double loanRequestTax) {
+    private LoanRequest(String loanRequestNumber, double loanRequestTax, BigDecimal loanAmount, int numberOfInstallments, LocalDate firstInstallmentDate, LocalDate registrationDate) {
         this.loanRequestNumber = loanRequestNumber;
         this.loanRequestTax = loanRequestTax;
+        this.loanAmount = loanAmount;
+        this.numberOfInstallments = numberOfInstallments;
+        this.firstInstallmentDate = firstInstallmentDate;
     }
 
-    static LoanRequest of(String loanRequestNumber, double loanRequestTax) {
-        return new LoanRequest(loanRequestNumber, loanRequestTax);
+    static LoanRequest of(String loanRequestNumber, double loanRequestTax, BigDecimal loanAmount, int numberOfInstallments, LocalDate firstInstallmentDate, LocalDate registrationDate) {
+        return new LoanRequest(loanRequestNumber, loanRequestTax, loanAmount, numberOfInstallments, firstInstallmentDate, registrationDate);
+    }
+
+    public String getLoanRequestNumber() {
+        return loanRequestNumber;
+    }
+
+    public double getLoanRequestTax() {
+        return loanRequestTax;
+    }
+
+    public BigDecimal getLoanAmount() {
+        return loanAmount;
+    }
+
+    public int getNumberOfInstallments() {
+        return numberOfInstallments;
+    }
+
+    public LocalDate getFirstInstallmentDate() {
+        return firstInstallmentDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof LoanRequest)) return false;
+        LoanRequest that = (LoanRequest) o;
+        return Objects.equals(loanRequestNumber, that.loanRequestNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(loanRequestNumber);
     }
 }
