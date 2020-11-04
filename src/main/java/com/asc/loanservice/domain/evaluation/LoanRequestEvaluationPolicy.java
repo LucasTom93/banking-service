cda39af7ee33c9ef88ec61d9dbd04e9580bda684
lukasz.tomczyk <lukasz.tomczyk93@gmail.com>
2020-11-04 16:09:47 +0100

    Added Policy domain annotation

--- src/main/java/com/asc/loanservice/domain/evaluation/LoanRequestEvaluationPolicy.java
+++ src/main/java/com/asc/loanservice/domain/evaluation/LoanRequestEvaluationPolicy.java
@@ -1,5 +1,8 @@
 package com.asc.loanservice.domain.evaluation;
 
+import com.asc.loanservice.annotations.Policy;
+
+@Policy
 public interface LoanRequestEvaluationPolicy {
 
     LoanRequestEvaluationResultDetails evaluate(EvaluationData evaluationData);
